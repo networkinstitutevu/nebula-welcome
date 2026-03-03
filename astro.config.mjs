1296cb57c66fb389b64a5a12fdff086f6a901b7d
Raiduy <radu.apsan@gmail.com>
2026-03-03 15:43:59 +0100

    Remove reference folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,104 +6,104 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE_URL || "http://localhost:4321",
-  base: process.env.SITE_BASE || "/welcome",
-  trailingSlash: 'ignore',
+    site: process.env.SITE_URL || "http://localhost:4321",
+    base: process.env.SITE_BASE || "/welcome",
+    trailingSlash: 'ignore',
 
-  integrations: [
-      starlight({
-          title: 'Welcome to Nebula',
-          logo: {
-              src: './src/assets/nebula-logo.png',
-              replacesTitle: true
-          },
-          social: [
-              {
-                  icon: 'puzzle',
-                  label: 'Nebula',
-                  href: 'https://nebula.cs.vu.nl',
-              },
-              {
-                  icon: 'github',
-                  label: 'GitHub',
-                  href: 'https://github.com/networkinstitutevu/nebula-welcome'
-              }
-          ],
-          sidebar: [
-              {
-                  label: 'Basic Use',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Getting to Know the Platform', slug: `gettingToKnowPlatform` },
-                      { label: 'Changing Password', slug: `changingPassword` },
-                      { label: 'Saving Chats', slug: `savingChats` },
-                      { label: 'Setting a default System Prompt', slug: `settingDefaultPrompts` },
-                      { label: 'Knowledge Bases', slug: `knowledgeBases` },
-                      { label: 'Models', slug: `models` },
-                      { label: 'Prompts', slug: `prompts` },
+    integrations: [
+        starlight({
+            title: 'Welcome to Nebula',
+            logo: {
+                src: './src/assets/nebula-logo.png',
+                replacesTitle: true
+            },
+            social: [
+                {
+                    icon: 'puzzle',
+                    label: 'Nebula',
+                    href: 'https://nebula.cs.vu.nl',
+                },
+                {
+                    icon: 'github',
+                    label: 'GitHub',
+                    href: 'https://github.com/networkinstitutevu/nebula-welcome'
+                }
+            ],
+            sidebar: [
+                {
+                    label: 'Basic Use',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Getting to Know the Platform', slug: `gettingToKnowPlatform` },
+                        { label: 'Changing Password', slug: `changingPassword` },
+                        { label: 'Saving Chats', slug: `savingChats` },
+                        { label: 'Setting a default System Prompt', slug: `settingDefaultPrompts` },
+                        { label: 'Knowledge Bases', slug: `knowledgeBases` },
+                        { label: 'Models', slug: `models` },
+                        { label: 'Prompts', slug: `prompts` },
 
-                  ],
-              },
-              {
-                  label: 'Advanced Use',
-                  items: [
-                      { label: 'Advanced Parameters', slug: `advancedParams` },
-                      { label: 'Modifying Advanced Parameters', slug: `modifyingAdvancedParams` },
-                  ]
-              },
-              {
-                  label: 'API Use',
-                  items: [
-                      { label: 'Generating an API Key', slug: `generatingApiKey` },
-                      { label: 'Using Nebula via API', slug: `usingNebulaViaApi` },
-                  ]
-              },
-              {
-                  label: 'Privacy',
-                  items: [
-                      { label: 'Encryption', slug: `encryption` },
-                      { label: 'Conversations', slug: `conversations` },
-                      { label: 'Knowledge Bases, Folders and System Prompts', slug: `knowledgeFoldersPrompts` },
-                      { label: 'Who can access the Nebula system logs?', slug: `whoCanAccess` },
-                  ]
-              },
-              {
-                  label: 'Security',
-                  items: [
-                      { label: 'Security', slug: `security` },
+                    ],
+                },
+                {
+                    label: 'Advanced Use',
+                    items: [
+                        { label: 'Advanced Parameters', slug: `advancedParams` },
+                        { label: 'Modifying Advanced Parameters', slug: `modifyingAdvancedParams` },
+                    ]
+                },
+                {
+                    label: 'API Use',
+                    items: [
+                        { label: 'Generating an API Key', slug: `generatingApiKey` },
+                        { label: 'Using Nebula via API', slug: `usingNebulaViaApi` },
+                    ]
+                },
+                {
+                    label: 'Privacy',
+                    items: [
+                        { label: 'Encryption', slug: `encryption` },
+                        { label: 'Conversations', slug: `conversations` },
+                        { label: 'Knowledge Bases, Folders and System Prompts', slug: `knowledgeFoldersPrompts` },
+                        { label: 'Who can access the Nebula system logs?', slug: `whoCanAccess` },
+                    ]
+                },
+                {
+                    label: 'Security',
+                    items: [
+                        { label: 'Security', slug: `security` },
 
-                  ]
-              },
-              {
-                  label: 'Backup',
-                  items: [
-                      { label: 'Backup', slug: `backup` },
+                    ]
+                },
+                {
+                    label: 'Backup',
+                    items: [
+                        { label: 'Backup', slug: `backup` },
 
-                  ]
-              },
-              {
-                  label: 'FAQ',
-                  items: [
-                      { label: 'FAQ', slug: `faq` },
+                    ]
+                },
+                {
+                    label: 'FAQ',
+                    items: [
+                        { label: 'FAQ', slug: `faq` },
 
-                  ]
-              },
-              {
-                  label: 'Getting Access to Nebula',
-                  items: [
-                      { label: 'Getting Access', slug: `gettingAccess` },
+                    ]
+                },
+                {
+                    label: 'Getting Access to Nebula',
+                    items: [
+                        { label: 'Getting Access', slug: `gettingAccess` },
 
-                  ]
-              },
-              {
-                  label: 'Reference',
-                  autogenerate: { directory: 'reference' },
-              },
-          ],
-      }),
-	],
+                    ]
+                },
+                // {
+                //     label: 'Reference',
+                //     autogenerate: { directory: 'reference' },
+                // },
+            ],
+        }),
+    ],
 
-  adapter: node({
-    mode: 'standalone',
-  }),
+    adapter: node({
+        mode: 'standalone',
+    }),
 });
